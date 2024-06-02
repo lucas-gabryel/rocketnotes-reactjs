a6b0973ec215835bd27c66a9a08a7ace062e44df
Lucas <lucas-gabryel@outlook.com>
2024-06-02 00:19:49 -0300

    styles in css

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,11 @@
+import "./style.css"
+import { Fragment } from "react"
+
+export function Details(){
+  return(
+    <Fragment>
+      <h1>Hello, World!</h1>
+      <span>Lucas Gabryel</span>
+    </Fragment>
+  )
+}
