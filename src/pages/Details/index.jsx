ac67602705dab4ecfab024db99d295caba070a24
Lucas <lucas-gabryel@outlook.com>
2024-06-02 11:36:27 -0300

    button component creation

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,12 @@
 import { Container } from "./style"
+import { Button } from "../../components/Button"
 
 export function Details(){
   return(
     <Container>
       <h1>Hello, World!</h1>
       <span>Lucas Gabryel</span>
+      <Button />
     </Container>
   )
 }
