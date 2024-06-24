b25f49f9f11518a3bad986c98478294fc3ff6c65
Lucas <lucas-gabryel@outlook.com>
2024-06-24 19:23:18 -0300

    create Section

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,20 @@
-import { Container } from "./style"
-import { Button } from "../../components/Button"
+import { Container, Links } from "./style"
 import { Header } from "../../components/Header"
+import { Button } from "../../components/Button"
+import { Section } from "../../components/Section"
 
 export function Details(){
   return(
     <Container>
       <Header/>
+
+      <Section title="Links úteis">
+      <Links>
+        <li><a href="#">www.rocketseat.com.br</a></li>
+        <li><a href="#">www.rocketseat.com.br</a></li>
+      </Links>
+      </Section>
+
       <Button title="Voltar"/>
     </Container>
   )
