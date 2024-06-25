40040a9d15d8dbe87beeb93babe50d66410e1cd2
Lucas <lucas-gabryel@outlook.com>
2024-06-24 21:29:50 -0300

    finalization of the details page

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Links } from "./style"
+import { Container, Links, Content } from "./style"
 import { Header } from "../../components/Header"
 import { Button } from "../../components/Button"
 import { Section } from "../../components/Section"
@@ -10,21 +10,27 @@ export function Details(){
     <Container>
       <Header/>
 
-      <ButtonText title="Excluir nota"/>
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota"/>
+          <h2>Introdução ao React</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde esse magni sint dolorem accusantium blanditiis facilis iste quibusdam illum repellat consequatur ut iusto ipsa hic exercitationem, optio libero possimus nesciunt?</p>
 
-      <Section title="Links úteis">
-      <Links>
-        <li><a href="#">www.rocketseat.com.br</a></li>
-        <li><a href="#">www.rocketseat.com.br</a></li>
-      </Links>
-      </Section>
+          <Section title="Links úteis">
+          <Links>
+            <li><a href="#">www.rocketseat.com.br</a></li>
+            <li><a href="#">www.rocketseat.com.br</a></li>
+          </Links>
+          </Section>
 
-      <Section title="Marcadores">
-      <Tag title="Express"></Tag>
-      <Tag title="Node"></Tag>
-      </Section>
+          <Section title="Marcadores">
+          <Tag title="Express"></Tag>
+          <Tag title="Node"></Tag>
+          </Section>
 
-      <Button title="Voltar"/>
+          <Button title="Voltar"/>
+        </Content>
+      </main>
     </Container>
   )
 }
