163a0f851fd061f1d9b9198ab49b73787e15bda1
Lucas <lucas-gabryel@outlook.com>
2024-06-25 19:01:19 -0300

    create Note and Error

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,6 +4,8 @@ import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 import { Header } from "../../components/Header"
 import { ButtonText } from "../../components/ButtonText"
 import { Input } from "../../components/Input"
+import { Section } from "../../components/Section"
+import { Note } from "../../components/Note"
 
 
 export function Home() {
@@ -26,7 +28,15 @@ export function Home() {
             </Search>
 
             <Content>
-
+                <Section title='Minhas notas'>
+                    <Note data={{
+                        title: 'React',
+                        tags: [
+                            {id: '1', name: 'react'},
+                            {id: '2', name: 'rocketseat'},
+                        ]
+                    }}/>
+                </Section>
             </Content>
 
             <NewNote>
