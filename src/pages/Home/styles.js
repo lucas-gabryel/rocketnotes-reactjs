965b869daef286b824b36c0d6accf5a7168fd169
Lucas <lucas-gabryel@outlook.com>
2024-06-25 10:42:01 -0300

    create menu section

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -19,12 +19,33 @@ export const Container  = styled.div`
 
 export const Brand  = styled.div`
     grid-area: brand;
-    background: red;
-`;
+    
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-bottom: 1px solid ${({ theme }) => theme.COLORS.BACKGROUND_700};
+    
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+
+    > h1 {
+        font-size: 24px;
+        color: ${({ theme }) => theme.COLORS.ORANGE};
+    }
+    `;
 
 export const Menu  = styled.ul`
     grid-area: menu;
-    background: green;
+
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+
+    padding-top: 64px;
+    text-align: center;
+
+    > li {
+        margin-bottom: 24px;
+    }
+    
 `;
 
 export const Search  = styled.div`
