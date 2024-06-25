3abec757ad3fd1a9d2896f5499e13c238de73f93
Lucas <lucas-gabryel@outlook.com>
2024-06-24 22:12:42 -0300

    create home page and grid definition

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Details } from './pages/Details' // problema com o nome deails, letra maiuscula e minuscula
+import { Home } from './pages/Home' // problema com o nome deails, letra maiuscula e minuscula
 import GlobalStyle from './styles/global';
 import { ThemeProvider } from 'styled-components';
 
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Details />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>,
 )
