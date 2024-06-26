b49fc35f810ef5e9404f47f21c1a38260d72dede
Lucas <lucas-gabryel@outlook.com>
2024-06-26 14:21:36 -0300

    create SignUp page

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Signin } from './pages/Signin' // problema com o nome deails, letra maiuscula e minuscula
+import { SignUp } from './pages/SignUp' // problema com o nome deails, letra maiuscula e minuscula
 import GlobalStyle from './styles/global';
 import { ThemeProvider } from 'styled-components';
 
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Signin />
+      <SignUp />
     </ThemeProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,18 +1,25 @@
 import { Container, Form, Background } from "./styles";
-import { FiMail, FiLock } from "react-icons/fi";
+import { FiMail, FiLock, FiUser } from "react-icons/fi";
 
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 import { ButtonText } from "../../components/ButtonText";
 
-export function SignIn(){
+export function SignUp(){
     return(
         <Container>
+            <Background />
             <Form>
                 <h1>Rocket Notes</h1>
                 <p>Aplicação para salvar e gerenciar seus links úteis.</p>
                 
-                <h2>Faça seu login</h2>
+                <h2>Crie sua conta</h2>
+
+                <Input 
+                 placeholder="Nome"
+                 type="text"
+                 icon={FiUser}
+                />
 
                 <Input 
                  placeholder="E-mail"
@@ -26,11 +33,10 @@ export function SignIn(){
                  icon={FiLock}
                 />
 
-                <Button title="Entrar"/>
-                <a href="#">Criar conta</a>
+                <Button title="Cadastrar"/>
+                <a href="#">Voltar para login</a>
                 {/* <ButtonText title="Criar conta"/> */}
             </Form>
-            <Background />
         </Container>
     )
 }
